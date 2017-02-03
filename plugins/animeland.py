bd0aedb1f1709a283d8fa9d5a93f93e04b396bfb
dend <dend456@gmail.com>
2017-02-03 03:06:54 -0500

    plugins

---
+++ plugins/animeland.py
@@ -0,0 +1,109 @@
+#! /usr/bin/env python3.6
+import bs4
+import requests
+import re
+import cfscrape
+
+
+display_name = 'Anime Land'
+categories = ['Dubbed', 'Movies']
+series_selector = '#ddmcc_container > div > ul > ul > li > a'
+dubbed_selector = '#meta > ul > li > a'
+
+headers = {}
+cookies = {}
+urls = {'base':   'http://www3.animeland.tv',
+        'Dubbed': 'http://www3.animeland.tv/anime-list',
+        'Movies': 'http://www3.animeland.tv/anime-movie'}
+
+series = {c: [] for c in categories}
+episode_cache = {c: dict() for c in categories}
+episode_re = re.compile('file: "(?P<url>.*)"')
+episode_loader_re = re.compile(r'#load"\).load\(\'(?P<url>\/ep\.php\?a=.+&id=.+)\'')
+
+
+def get_series_from_url(url):
+    s = []
+    res = requests.get(url, headers=headers, cookies=cookies)
+    if res:
+        soup = bs4.BeautifulSoup(res.text, 'lxml')
+        nodes = soup.select(series_selector)
+        for node in nodes:
+            title = node.contents[0]
+            link = node['href']
+            s.append((title, link))
+
+    return sorted(s)
+
+
+def get_movies(series_name):
+    pass
+
+
+def get_series_episodes(category, series_name):
+    series_cache = episode_cache.get(category, None)
+    if series_cache is not None:
+        episodes = series_cache.get(series_name, None)
+        if episodes:
+            return episodes
+    else:
+        return []
+
+    episodes = []
+    selected_series = series[category]
+
+    for link in selected_series:
+        if link[0] == series_name:
+            res = requests.get(link[1], headers=headers, cookies=cookies)
+            if res:
+                matches = episode_loader_re.findall(res.text)
+                if matches:
+                    loader_url = urls['base'] + matches[0]
+                    res = requests.get(loader_url, headers=headers, cookies=cookies)
+                    if res:
+                        soup = bs4.BeautifulSoup(res.text, 'lxml')
+                        nodes = soup.select(dubbed_selector)
+                        for node in nodes:
+                            episodes.append((node.contents[0], urls['base'] + node['href']))
+                        episodes.reverse()
+                        series_cache[series_name] = episodes
+                        return episodes
+    return episodes
+
+
+def get_video_url(category, series_name, episode_title):
+    link = None
+    for l in episode_cache[category][series_name]:
+        if l[0] == episode_title:
+            link = l[1]
+            break
+
+    if not link:
+        return None
+
+    res = requests.get(link, headers=headers, cookies=cookies)
+    if res:
+        soup = bs4.BeautifulSoup(res.text, 'lxml')
+        iframe = soup.select('iframe#video')
+        if iframe:
+            frame_url = urls['base'] + iframe[0]['src']
+
+        res = requests.get(frame_url, headers=headers, cookies=cookies)
+        if res:
+            matches = episode_re.findall(res.text)
+            if matches:
+                return matches[0]
+    return None
+
+
+def load():
+    global cookies, headers
+    cookies, user_agent = cfscrape.get_tokens(urls['base'])
+    headers = {'User-Agent': user_agent}
+    series['Dubbed'] = get_series_from_url(urls['Dubbed'])
+    series['Movies'] = ('Movies', urls['Movies'])
+
+if __name__ == '__main__':
+    load()
+    print(get_series_episodes('Dubbed', '009-1'))
+    print(get_video_url('Dubbed', '009-1', 'Episode 3'))
---
+++ plugins/watchcartoononline.py
@@ -0,0 +1,119 @@
+#! /usr/bin/env python3.6
+import bs4
+import requests
+import re
+
+
+display_name = 'Watch Cartoon Online'
+categories = ['Dubbed', 'Subbed', 'Cartoon', 'OVA', 'Movies']
+series_selector = '#ddmcc_container > div > ul > ul > li > a'
+
+urls = {'Dubbed': 'https://www.watchcartoononline.io/dubbed-anime-list',
+        'Subbed': 'https://www.watchcartoononline.io/subbed-anime-list',
+        'Cartoon': 'https://www.watchcartoononline.io/cartoon-list',
+        'OVA': 'https://www.watchcartoononline.io/ova-list',
+        'Movies': 'https://www.watchcartoononline.io/movie-list'}
+
+series = {c: [] for c in categories}
+episode_cache = {c: dict() for c in categories}
+episode_re = re.compile('file: "(?P<url>.*)"')
+
+
+def get_series_from_url(url):
+    s = []
+    res = requests.get(url)
+    if res:
+        soup = bs4.BeautifulSoup(res.text, 'lxml')
+        nodes = soup.select(series_selector)
+        for node in nodes:
+            title = node.contents[0]
+            link = node['href']
+            s.append((title, link))
+
+    return sorted(s)
+
+
+def get_movies_or_ovas(category, series_name):
+    pass
+
+
+def get_series_episodes(category, series_name):
+    series_cache = episode_cache.get(category, None)
+    if series_cache is not None:
+        episodes = series_cache.get(series_name, None)
+        if episodes:
+            return episodes
+    else:
+        return []
+
+    episodes = []
+    selected_series = series[category]
+
+    for link in selected_series:
+        if link[0] == series_name:
+            res = requests.get(link[1])
+            if res:
+                soup = bs4.BeautifulSoup(res.text, 'lxml')
+                tables = soup.select('#sidebar > table')
+                nodes = tables[0].select('tr > td > div > div > li > a')
+                for node in nodes:
+                    episodes.append((node.contents[0], node['href']))
+            episodes.reverse()
+            series_cache[series_name] = episodes
+            return episodes
+    return episodes
+
+
+def find_best_quality(urls):
+    tests = ['1080p', '720p']
+    for test in tests:
+        for url in urls:
+            if test in url:
+                return url
+    return urls[0]
+
+
+def get_video_url(category, series_name, episode_title):
+    link = None
+    for l in episode_cache[category][series_name]:
+        if l[0] == episode_title:
+            link = l[1]
+            break
+
+    if not link:
+        return None
+
+    res = requests.get(link)
+    if res:
+        soup = bs4.BeautifulSoup(res.text, 'lxml')
+        iframes = soup.select('iframe')
+        frame_url = None
+        for frame in iframes:
+            if frame.has_attr('id') and frame['id'].startswith('frame'):
+                frame_url = frame['src']
+                break
+
+        if not frame_url:
+            return None
+
+        post_data = {'fuck_you': '', 'confirm': 'Click+Here+to+Watch+Free%21%21'}
+        res = requests.post(frame_url, data=post_data)
+        if res:
+            matches = episode_re.findall(res.text)
+            if matches:
+                return find_best_quality(matches)
+    return None
+
+
+def load():
+    series['Dubbed'] = get_series_from_url(urls['Dubbed'])
+    series['Subbed'] = get_series_from_url(urls['Subbed'])
+    series['Cartoon'] = get_series_from_url(urls['Cartoon'])
+    series['Movies'] = ('Movies', urls['Movies'])
+    series['OVA'] = ('OVA', urls['OVA'])
+
+if __name__ == '__main__':
+    load()
+    get_series_episodes('Dubbed', 'Fairy Tail (Official Dub)')
+    print(get_video_url('Dubbed', 'Fairy Tail (Official Dub)', 'Fairy Tail Episode 252 English Dubbed'))
+    s4 = 2
